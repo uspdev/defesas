a2261792b21fd4225bd2ad5261b4f0588296d205
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2020-09-14 12:05:54 -0300

    Ajuste de Ordenaçao - Defesas defendidas

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -22,10 +22,10 @@ class indexController extends Controller
 
     public function anteriores(Request $request){
         if($request->busca != '') {
-            $agendamentos = Agendamento::where('area_programa', '=', $request->busca)->where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'asc')->paginate(20);
+            $agendamentos = Agendamento::where('area_programa', '=', $request->busca)->where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'desc')->paginate(20);
         } 
         else{
-            $agendamentos = Agendamento::where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'asc')->paginate(20);
+            $agendamentos = Agendamento::where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'desc')->paginate(20);
         }
         if ($agendamentos->count() == null) {
             $request->session()->flash('alert-danger', 'Não há registros!');
