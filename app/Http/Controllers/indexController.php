0817c801cc9a347d234d938d4efb96ded8c2b73a
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2020-10-02 10:28:39 -0300

    WIP - Filtros de busca

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -9,22 +9,20 @@ use App\Models\Docente;
 class indexController extends Controller
 {
     public function index(Request $request){
-        $query = Agendamento::where('data_horario','>=',date('Y-m-d H:i:s'))->orderBy('data_horario', 'asc');
-        $query2 = Docente::orderBy('nome', 'asc');
-        if($request->busca != ''){
-            $query->where('nome', 'LIKE', "%$request->busca%");
-            $query->orWhere('titulo', 'LIKE', "%$request->busca%");
-            $query2->where('nome', 'LIKE', "%$request->busca%");
-            foreach($query2->get() as $orientador){
-                $query->orWhere('orientador', '=', $orientador->n_usp);
-            }
+        $query = Agendamento::join('docentes', 'docentes.n_usp', '=', 'agendamentos.orientador')->where('agendamentos.data_horario','>=',date('Y-m-d H:i:s'))->orderBy('agendamentos.data_horario', 'asc')->select('agendamentos.*');
+        if($request->nivel != '' and $request->busca_nivel != '') {
+            $query->where('agendamentos.nivel', '=', $request->busca_nivel);
         }
         if($request->programa != '' and $request->busca_programa != '') {
-            $query->where('area_programa', '=', $request->busca_programa);
-        }
-        if($request->nivel != '' and $request->busca_nivel != '') {
-            $query->where('nivel', '=', $request->busca_nivel);
+            $query->where('agendamentos.area_programa', '=', $request->busca_programa);
         } 
+        if($request->busca != ''){
+            $query->where('docentes.nome', 'LIKE', "%$request->busca%");
+            $query->orWhere('agendamentos.nome', 'LIKE', "%$request->busca%");
+            $query->orWhere('agendamentos.titulo', 'LIKE', "%$request->busca%");
+            
+        }
+        
         $agendamentos = $query->paginate(20);
 
         if ($agendamentos->count() == null) {
@@ -34,22 +32,19 @@ class indexController extends Controller
     }
 
     public function anteriores(Request $request){
-        $query = Agendamento::where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'desc');
-        $query2 = Docente::orderBy('nome', 'asc');
-        if($request->busca != '') {
-            $query->where('nome', 'LIKE', "%$request->busca%");
-            $query->orWhere('titulo', 'LIKE', "%$request->busca%");
-            $query2->where('nome', 'LIKE', "%$request->busca%");
-            foreach($query2->get() as $orientador){
-                $query->orWhere('orientador', '=', $orientador->n_usp);
-            }
+        $query = Agendamento::join('docentes', 'docentes.n_usp', '=', 'agendamentos.orientador')->where('data_horario','<',date('Y-m-d H:i:s'))->orderBy('data_horario', 'desc')->select('agendamentos.*');
+        if($request->nivel != '' and $request->busca_nivel != '') {
+            $query->where('agendamentos.nivel', '=', $request->busca_nivel);
         }
-        if($request->programa == 'on') {
-            $query->where('area_programa', $request->busca_programa);
-        }
-        if($request->nivel == 'on') {
-            $query->where('nivel', $request->busca_nivel);
+        if($request->programa != '' and $request->busca_programa != '') {
+            $query->where('agendamentos.area_programa', '=', $request->busca_programa);
         } 
+        if($request->busca != '') {
+            $query->where('agendamentos.nome', 'LIKE', "%$request->busca%");
+            $query->orWhere('agendamentos.titulo', 'LIKE', "%$request->busca%");
+            $query->orWhere('docentes.nome', 'LIKE', "%$request->busca%");
+        }
+        
         $agendamentos = $query->paginate(20);
 
         if ($agendamentos->count() == null) {
@@ -58,3 +53,4 @@ class indexController extends Controller
         return view('anteriores')->with('agendamentos',$agendamentos);
     }
 }
+
