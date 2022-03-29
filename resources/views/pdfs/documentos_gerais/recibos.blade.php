1fb63b594ded4746458d1578390d1fd22e3b5cd2
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2022-03-29 20:10:42 -0300

    Ajusta Documento Zero/Declarações/Statements e Recibo de remessa de documentos

--- resources/views/pdfs/documentos_gerais/recibos.blade.php
+++ resources/views/pdfs/documentos_gerais/recibos.blade.php
@@ -35,83 +35,84 @@
 @section('content')
 	@inject('pessoa','Uspdev\Replicado\Pessoa')
 	@foreach($professores as $professor)
-		<table width="18cm" class="negrito">
-			<tr>
-				<td> RECIBO DE REMESSA DE DOCUMENTOS </td> 
-				<td  width="4cm"> <u>DATA:</u> </td> 
-			</tr> 
-		</table> 
-		<table width="18cm" class="negrito">
-			<tr>
-				<td> 
-					<u>DO:</u> SERVIÇO DE PÓS-GRADUAÇÃO DA FFLCH <br>
-					<u>PARA:</u> {{$agendamento->dadosProfessor($professor->codpes)->endereco ?? ' '}}, {{$agendamento->dadosProfessor($professor->codpes)->bairro ?? ' '}} <br>
-    				CEP:{{$agendamento->dadosProfessor($professor->codpes)->cep ?? ' '}} - {{$agendamento->dadosProfessor($professor->codpes)->cidade ?? ' '}}/{{$agendamento->dadosProfessor($professor->codpes)->estado ?? ' '}}
-					<div style="text-indent:1.5cm;">
-						A/C: Prof(a). Dr(a). {{$agendamento->dadosProfessor($professor->codpes)->nome ?? 'Professor não cadastrado'}}
-					</div> 
-				</td> 
-			</tr> 
-		</table>
-		<table width="18cm" class="negrito">
-			<tr>
-				<td> 
-					<u>ASSUNTO:</u> encaminho exemplar do trabalho do aluno(a) Sr(a). {{$agendamento->nome}}
-				</td> 
-			</tr>
-		</table>
+		@if($agendamento->dadosProfessor($professor->codpes)->docente_usp == 'sim')
+			<table width="18cm" class="negrito">
+				<tr>
+					<td> RECIBO DE REMESSA DE DOCUMENTOS </td> 
+					<td  width="4cm"> <u>DATA:</u> </td> 
+				</tr> 
+			</table> 
+			<table width="18cm" class="negrito">
+				<tr>
+					<td> 
+						<u>DO:</u> SERVIÇO DE PÓS-GRADUAÇÃO DA FFLCH <br>
+						<u>PARA:</u> {{$agendamento->dadosProfessor($professor->codpes)->endereco ?? ' '}}, {{$agendamento->dadosProfessor($professor->codpes)->bairro ?? ' '}} <br>
+							CEP:{{$agendamento->dadosProfessor($professor->codpes)->cep ?? ' '}} - {{$agendamento->dadosProfessor($professor->codpes)->cidade ?? ' '}}/{{$agendamento->dadosProfessor($professor->codpes)->estado ?? ' '}}
+						<div style="text-indent:1.5cm;">
+							A/C: Prof(a). Dr(a). {{$agendamento->dadosProfessor($professor->codpes)->nome ?? 'Professor não cadastrado'}}
+						</div> 
+					</td> 
+				</tr> 
+			</table>
+			<table width="18cm" class="negrito">
+				<tr>
+					<td> 
+						<u>ASSUNTO:</u> encaminho exemplar do trabalho do aluno(a) Sr(a). {{$agendamento->nome}}
+					</td> 
+				</tr>
+			</table>
 
-		<table width="18cm" class="negrito">
-			<tr>
-				<td width="9cm">
-					Recebido em: ____/____/____
-				</td> 
-				<td>
-					<div class="assinatura">&nbsp;</div>
-					<div class="nome">Nome Legível</div>
-				</td> 
-			</tr> 
-		</table>
-		
-		<br><br><br><br><br><br><br><br><br><br>
+			<table width="18cm" class="negrito">
+				<tr>
+					<td width="9cm">
+						Recebido em: ____/____/____
+					</td> 
+					<td>
+						<div class="assinatura">&nbsp;</div>
+						<div class="nome">Nome Legível</div>
+					</td> 
+				</tr> 
+			</table>
+			
+			<br><br><br><br><br><br><br><br><br><br>
 
-		<table width="18cm" class="negrito">
-			<tr>
-				<td> RECIBO DE REMESSA DE DOCUMENTOS </td> 
-				<td  width="4cm"> <u>DATA:</u> </td> 
-			</tr> 
-		</table>
+			<table width="18cm" class="negrito">
+				<tr>
+					<td> RECIBO DE REMESSA DE DOCUMENTOS </td> 
+					<td  width="4cm"> <u>DATA:</u> </td> 
+				</tr> 
+			</table>
 
-		<table width="18cm" class="negrito">
-			<tr>
-				<td>
-					<u>DO:</u> SERVIÇO DE PÓS-GRADUAÇÃO DA FFLCH <br>
-					<u>PARA:</u>  {{$agendamento->dadosProfessor($professor->codpes)->endereco ?? ' '}}, {{$agendamento->dadosProfessor($professor->codpes)->bairro ?? ' '}} <br>
-    				CEP:{{$agendamento->dadosProfessor($professor->codpes)->cep ?? ' '}} - {{$agendamento->dadosProfessor($professor->codpes)->cidade ?? ' '}}/{{$agendamento->dadosProfessor($professor->codpes)->estado ?? ' '}}
-					<div style="text-indent:1.5cm;"> A/C: Prof(a). Dr(a). {{$agendamento->dadosProfessor($professor->codpes)->nome ?? 'Professor não cadastrado'}} </div> 
-				</td> 
-			</tr>
-		</table>
+			<table width="18cm" class="negrito">
+				<tr>
+					<td>
+						<u>DO:</u> SERVIÇO DE PÓS-GRADUAÇÃO DA FFLCH <br>
+						<u>PARA:</u>  {{$agendamento->dadosProfessor($professor->codpes)->endereco ?? ' '}}, {{$agendamento->dadosProfessor($professor->codpes)->bairro ?? ' '}} <br>
+							CEP:{{$agendamento->dadosProfessor($professor->codpes)->cep ?? ' '}} - {{$agendamento->dadosProfessor($professor->codpes)->cidade ?? ' '}}/{{$agendamento->dadosProfessor($professor->codpes)->estado ?? ' '}}
+						<div style="text-indent:1.5cm;"> A/C: Prof(a). Dr(a). {{$agendamento->dadosProfessor($professor->codpes)->nome ?? 'Professor não cadastrado'}} </div> 
+					</td> 
+				</tr>
+			</table>
 
-		<table width="18cm" class="negrito">
-			<tr>
-				<td> 
-					<u>ASSUNTO:</u> encaminho exemplar do trabalho do aluno(a) Sr(a). {{$agendamento->nome}}
-				</td> 
-			</tr>
-		</table>
-		
-		<table width="18cm" class="negrito">
-			<tr>
-				<td width="9cm">Recebido em: ____/____/____ </td> 
-				<td>
-					<div class="assinatura">&nbsp;</div>
-					<div class="nome">Nome Legível</div>
-				</td> 
-			</tr>
-		</table>
+			<table width="18cm" class="negrito">
+				<tr>
+					<td> 
+						<u>ASSUNTO:</u> encaminho exemplar do trabalho do aluno(a) Sr(a). {{$agendamento->nome}}
+					</td> 
+				</tr>
+			</table>
+			
+			<table width="18cm" class="negrito">
+				<tr>
+					<td width="9cm">Recebido em: ____/____/____ </td> 
+					<td>
+						<div class="assinatura">&nbsp;</div>
+						<div class="nome">Nome Legível</div>
+					</td> 
+				</tr>
+			</table>
 
-		<p style="page-break-before: always">&nbsp;</p> 
-
+			<p class="page-break"></p> 
+		@endif
 	@endforeach
 @endsection('content')
