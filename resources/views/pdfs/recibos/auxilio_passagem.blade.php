e294d7fa6b6db85c2bc05af7ac130ee9125dca9b
Ricardo Fontoura <ricardfo@usp.br>
2025-04-15 10:40:06 -0300

    ajustes no pdf auxilio passagem aérea

--- resources/views/pdfs/recibos/auxilio_passagem.blade.php
+++ resources/views/pdfs/recibos/auxilio_passagem.blade.php
@@ -12,31 +12,35 @@
 
 @section('content')
     <br>
-    <left> {!! $configs->header_auxilio !!} </left> 
-    <left> <u><b>{{$dados->processo}}</b></u> </left> <br> 
+    <left> {!! $configs->header_auxilio !!} </left>
+    <left> <u><b>{{ $dados->processo }}</b></u> </left> <br>
 
-    <p> Banca de defesa de <b> {{$agendamento->nome}} </p> 
-    <p> Passageiro(a) {{$agendamento->dadosProfessor($banca->codpes)->nome ?? 'Professor não cadastrado'}} </p> 
-    <p> Itinerário: {{$dados->itinerario}} </p> 
-    <p> Partida: {{$dados->partida}}</p> 
-    <p> Retorno: {{$dados->retorno}} </p> 
-    <p> Telefone: {{$agendamento->dadosProfessor($banca->codpes)->telefone ?? ' '}} </p> 
-    <p> E-mail: {{$agendamento->dadosProfessor($banca->codpes)->email ?? ' '}}
-    </p> 
+    <p> Banca de defesa de <b> {{ $agendamento['nome'] }} </p>
+    <p> Passageiro(a) {{ $docente['nompesttd'] ?? 'Professor não cadastrado'}} </p>
+    <p> Itinerário: {{ $dados->itinerario }} </p>
+    <p> Partida: {{ $dados->partida }}</p>
+    <p> Retorno: {{ $dados->retorno }} </p>
+    <p> Telefones:
+      @foreach ($docente['telefones'] as $telefone)
+        {{ $telefone }}
+      @endforeach
+    </p>
+    <p> E-mail: {{ $docente['email'] }}
+    </p>
     <div class="justificar"> {!! $configs->obs_passagem !!} </div>
-            
-    <p> 
+
+    <p>
       @php(setlocale(LC_TIME, 'pt_BR','pt_BR.utf-8','portuguese'))
       São Paulo, {{ strftime('%d de %B de %Y', strtotime('today')) }}
     </p>
-    <center> 
+    <center>
       <br>
       <p style="margin-top:3cm;">
-      <b> 
-        {{Auth::user()->name}} <br>
-        {{Auth::user()->codpes}}
-        <br> @if($pessoa::cracha(Auth::user()->codpes)) SPG-{{$pessoa::cracha(Auth::user()->codpes)['nomorg']}}-USP @endif 
-      </b> 
+      <b>
+        {{ Auth::user()->name }} <br>
+        {{ Auth::user()->codpes }}
+        <br> @if($pessoa::cracha(Auth::user()->codpes)) SPG-{{$pessoa::cracha(Auth::user()->codpes)['nomorg']}}-USP @endif
+      </b>
       </p>
     </center>
 @endsection('content')
