22fced010346edeff3a57b2d70939af0eb77c7a1
Ricardo Fontoura <ricardfo@usp.br>
2025-04-15 10:19:13 -0300

    ajustes no pdf requisição passagem aérea

--- resources/views/pdfs/recibos/passagem.blade.php
+++ resources/views/pdfs/recibos/passagem.blade.php
@@ -17,7 +17,7 @@
         border: 1px #000 solid; padding: 1
     }
     .data_hoje{
-        margin-left: 11cm; margin-bottom:0.8cm; 
+        margin-left: 11cm; margin-bottom:0.8cm;
     }
     .justificar{
         text-align: justify; width: 15cm; margin-left:0.5cm;
@@ -36,44 +36,48 @@
     @php(setlocale(LC_TIME, 'pt_BR','pt_BR.utf-8','portuguese'))
     São Paulo, {{ strftime('%d de %B de %Y', strtotime('today')) }}
   </div>
-	<left> {!! $configs->agencia_viagem !!} </left> 
-	<br> 
-    <ul> 
-		<li>Requisição passagem aérea nº <b>{{$dados->requisicao}} </b> </li>  
-		<li>Programa: <b>{{$agendamento->nome_area}} </b> </li>  
-    <li><b>{{$agendamento->nivel}} </b> </li>  
-		<li>Defesa do Sr.(a): <b> {{$agendamento->nome}} </b> </li>  
-		<li>Orientador(a): Prof(a) Dr(a)<b> {{$agendamento->nome_orientador ?? $pessoa::dump($agendamento->orientador)['nompes']}} @if($agendamento->co_orientador) e {{$agendamento->nome_co_orientador ?? $agendamento->dadosProfessor($agendamento->co_orientador)->nome}} @endif </b> </li>
+	<left> {!! $configs->agencia_viagem !!} </left>
+	<br>
+    <ul>
+		<li>Requisição passagem aérea nº <b>{{ $dados->requisicao }} </b> </li>
+		<li>Programa: <b>{{ $agendamento['area']['nomare'] }} </b> </li>
+    <li><b>{{ $agendamento['nivel'] }} </b> </li>
+		<li>Defesa do Sr.(a): <b> {{ $agendamento['nome'] }} </b> </li>
+		<li>Orientador(a): Prof(a) Dr(a)<b> {{ $agendamento['orientador'] }} </b> </li>
     </ul>
 	<div class="justificar" style="text-indent:1cm;" >{!! $configs->agencia_texto !!} </div>
-	<div class="importante">  
-		Interessado(a): Prof(a). Dr(a). <b> {{$agendamento->dadosProfessor($banca->codpes)->nome ?? 'Professor não cadastrado'}}</b> <br>
-		E-mail: <b>{{$agendamento->dadosProfessor($banca->codpes)->email ?? ' '}}</b> <br>
-		Telefone:<b> {{$agendamento->dadosProfessor($banca->codpes)->telefone ?? ' '}} </b> <br>
-		Data da defesa:<b> {{date('d/m/Y', strtotime($agendamento->data_horario))}}</b> <br>
-		Trajeto da passagem aérea <b> {{$dados->trajeto}}</b> <br>
+	<div class="importante">
+		Interessado(a): Prof(a). Dr(a). <b> {{ $docente['nompesttd'] ?? 'Professor não cadastrado' }}</b> <br>
+		E-mail: <b>{{ $docente['email'] ?? ' ' }}</b> <br>
+    Telefones:<b>
+      @foreach ($docente['telefones'] as $telefone)
+        {{ $telefone }}
+      @endforeach
+    </b> <br>
+		Data da defesa:<b> {{ date('d/m/Y', strtotime($agendamento->data_horario)) }}</b> <br>
+		Trajeto da passagem aérea <b> {{ $dados->trajeto }}</b> <br>
 	</div> <br>
-	<table style="width:15.5cm; text-align:center;"> 
-		<tr> 
+	<table style="width:15.5cm; text-align:center;">
+		<tr>
 			<td style="width:7.25cm;"> <p style="text-align:center;"> <b>IDA </b></p></td>
 			<td> <p style="text-align:center;"><b>VOLTA </b> </p> </td>
 		</tr>
 		<tr>
-			<td> {{$dados->ida}} </td>
-			<td> {{$dados->volta}} </td>
+			<td> {{ $dados->ida }} </td>
+			<td> {{ $dados->volta }} </td>
 		</tr>
 	</table> <br>
 
-	<p class="justificar"><b> Solicitamos a gentileza no sentido de comunicar aos professores interessados, com antecedência, n° do PTA e nome da companhia aérea.  </b></p> <br> <br> 
+	<p class="justificar"><b> Solicitamos a gentileza no sentido de comunicar aos professores interessados, com antecedência, n° do PTA e nome da companhia aérea.  </b></p> <br> <br>
 	<p style="text-align:center;"><b> Favor faturar para: {!! $configs->faturar_para !!} </b></p> <br>
-  <div style="margin-top:0.5em; text-align:center;"> 
-      Atenciosamente, 
+  <div style="margin-top:0.5em; text-align:center;">
+      Atenciosamente,
   </div>
- 	<p style="text-align:center;"> 
-    <b> 
-		  {{Auth::user()->name}}
-      <br> 
-      Serviço de Pós-Graduação - @if($pessoa::cracha(Auth::user()->codpes)) {{$pessoa::cracha(Auth::user()->codpes)['nomorg']}}/USP @endif 
+ 	<p style="text-align:center;">
+    <b>
+		  {{ Auth::user()->name }}
+      <br>
+      Serviço de Pós-Graduação - @if($pessoa::cracha(Auth::user()->codpes)) {{$pessoa::cracha(Auth::user()->codpes)['nomorg']}}/USP @endif
 		</b>
   </p>
 @endsection('content')
