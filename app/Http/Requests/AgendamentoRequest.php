02a25fc20784001bd71c82bcd45c33c9f440dec4
Ricardo Fontoura <ricardfo@usp.br>
2025-05-09 11:35:18 -0300

    alterando validação do link sala virtual

--- app/Http/Requests/AgendamentoRequest.php
+++ app/Http/Requests/AgendamentoRequest.php
@@ -3,10 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Carbon\Carbon;
 use App\Models\Agendamento;
 use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\Validator;
 
 class AgendamentoRequest extends FormRequest
 {
@@ -35,7 +33,7 @@ class AgendamentoRequest extends FormRequest
                 'required',
                 Rule::in(Agendamento::tipos())
             ],
-            'sala_virtual' => 'required_if:tipo,Virtual'
+            'sala_virtual' => 'nullable'
         ];
 
         if ($this->method() == 'POST') {
@@ -45,42 +43,7 @@ class AgendamentoRequest extends FormRequest
         }
 
         return $rules;
-
-        /* $agendamento = new Agendamento; */
-        /* $rules = [ */
-        /*     'codpes' => 'required|integer|codpes', */
-        /*     'nome' => '', */
-        /*     'regimento' => ['required',Rule::in($agendamento->regimentoOptions())], */
-        /*     'orientador_votante' => ['required',Rule::in($agendamento->orientadorvotanteOptions())], */
-        /*     'nivel' => ['required',Rule::in($agendamento->nivelOptions())], */
-        /*     'titulo' => 'required|max:2000', */
-        /*     'title' => 'nullable|max:2000', */
-        /*     'area_programa' => ['required',Rule::in(Agendamento::devolverCodProgramas())], */
-        /*     'sala' => 'required', */
-        /*     'data' => 'required|date_format:d/m/Y', */
-        /*     'horario' => 'required|date_format:H:i', */
-        /*     'orientador' => 'required|integer|codpes', */
-        /*     'nome_orientador' => 'nullable', */
-        /*     'tipo' => ['required',Rule::in($agendamento->tipos())], */
-        /*     'co_orientador' => 'nullable|integer|codpes', */
-        /*     'nome_co_orientador' => 'nullable', */
-        /*     'resumo' => 'nullable', */
-        /*     'enviar_email' => 'nullable', */
-        /*     'sala_virtual' => 'nullable', */
-        /*     'approval_status' => [ */
-        /*         'nullable', */
-        /*         Rule::in(Agendamento::statusApprovalOptions()), */
-        /*     ], */
-        /* ]; */
-        /* return $rules; */
     }
-
-    /* public function validated($key = null, $default = null) */
-    /* { */
-    /*     return array_merge(parent::validated(), [ */
-    /*         'data_horario' => Carbon::createFromFormat('d/m/Y H:i', $this->data . $this->horario)->format('Y-m-d H:i'), */
-    /*     ]); */
-    /* } */
 
     public function messages(){
         return [
@@ -92,27 +55,6 @@ class AgendamentoRequest extends FormRequest
             'horario.date_format' => 'Insira um horário válido no formato H:m',
             'sala.required' => 'A sala é obrigatório',
             'tipo.required' => 'Escolha o tipo da defesa',
-            'sala_virtual.required_if' => 'O link da sala virtual é obrigatório se o tipo da defesa for Virtual'
         ];
-
-        /* return[ */
-        /*     'codpes.required' => 'O preenchimento do Número USP é obrigatório.', */
-        /*     'nome' => '', */
-        /*     'regimento.required' => 'É necessário selecionar o regimento em que se enquadra.', */
-        /*     'orientador_votante.required' => 'É necessário selecionar se o Orientador é um Orientador Votante.', */
-        /*     'nivel.required' => 'É necessário a seleção do nível da defesa.', */
-        /*     'titulo.required' => 'É obrigatório o preenchimento do título da defesa.', */
-        /*     'area_programa.required' => 'É necessário a seleção do programa em que se enquadra a defesa.', */
-        /*     'sala.required' => 'É necessário indicar a sala da defesa.', */
-        /*     'data.required' => 'É necessário indicar da data da defesa', */
-        /*     'data.date_format' => 'Insira a data no formato d/m/a', */
-        /*     'horario.required' => 'É necessário indicar o horário da defesa', */
-        /*     'horario.date_format' => 'Insira um horário válido', */
-        /*     'orientador.required' => 'O preenchimento do Número USP do Orientador é obrigatório.', */
-        /*     'tipo.required' => 'O preenchimento do Tipo da defesa é obrigatório.', */
-        /*     'titulo.max' => 'Ultrapassou o número máximo de 2000 caracteres no título.', */
-        /*     'title.max' => 'Ultrapassou o número máximo de 2000 caracteres no título em inglês.', */
-        /*     'enviar_email.nullable' => 'Você terá que enviar aos responsáveis o link da sala virtual' */
-        /* ]; */
     }
 }
